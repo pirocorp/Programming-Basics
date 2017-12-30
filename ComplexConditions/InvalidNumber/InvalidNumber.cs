a98dbd21dfdd53603596be69c5edc293154f266d
pirocorp <pirocorp@abv.bg>
2017-12-30 14:53:42 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InvalidNumber
+{
+    class InvalidNumber
+    {
+        static void Main(string[] args)
+        {
+            var num = double.Parse(Console.ReadLine());
+            var inRange = (num >= 100 && num <= 200) || num == 0;
+            if (!inRange)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
