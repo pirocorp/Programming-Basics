db709e25b3021834c2991c8ae1f9c32dedb90c75
pirocorp <pirocorp@abv.bg>
2017-12-30 14:55:49 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/Volleyball/Volleyball.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Volleyball
+{
+    class Volleyball
+    {
+        static void Main(string[] args)
+        {
+            var year = Console.ReadLine().ToLower();
+            var holidays = int.Parse(Console.ReadLine());
+            var weekendsHome = int.Parse(Console.ReadLine());
+
+            int sofiaWeekends = 48 - weekendsHome;
+            double playSofia = 3.0 * sofiaWeekends / 4 + 2.0 * holidays / 3;
+            double playTotal = playSofia + weekendsHome;
+
+            if (year == "leap")
+            {
+                playTotal = Math.Floor((playTotal * 1.15));
+
+            }
+            else if (year == "normal")
+            {
+                playTotal = Math.Floor(playTotal);
+            }
+            Console.WriteLine(playTotal);
+        }
+    }
+}
