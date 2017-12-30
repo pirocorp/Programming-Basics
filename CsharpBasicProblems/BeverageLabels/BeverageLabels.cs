4017e975bad9751b6ba691518c1de963cc9d26c5
pirocorp <pirocorp@abv.bg>
2017-12-30 16:59:48 +0100

    Calculate the energy and sugar content for the given volume

---
+++ CsharpBasicProblems/BeverageLabels/BeverageLabels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BeverageLabels
+{
+    class BeverageLabels
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energyContentPer100ml = int.Parse(Console.ReadLine());
+            var sugarContentPer100ml = int.Parse(Console.ReadLine());
+
+            var volumeRatioPer100ml = (volume / 100.0);
+            var energy = volumeRatioPer100ml * energyContentPer100ml;
+            var sugar = volumeRatioPer100ml * sugarContentPer100ml;
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
