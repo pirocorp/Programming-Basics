a1f5fd3a81cbae3f0259c07de756202f4003210b
pirocorp <pirocorp@abv.bg>
2017-12-30 19:12:42 +0100

    Take as an input an odd number and print the absolute value of it. If the number is even, print "Please write an odd number." and continue reading numbers.

---
+++ CsharpBasicProblems/OddNumber/OddNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OddNumber
+{
+    class OddNumber
+    {
+        static void Main()
+        {
+            int n;
+            while(true)
+            {
+                n = Math.Abs(int.Parse(Console.ReadLine()));
+                if (n % 2 == 1) break;
+                Console.WriteLine("Please write an odd number.");
+            }
+            Console.WriteLine("The number is: " + n);
+        }
+    }
+}
