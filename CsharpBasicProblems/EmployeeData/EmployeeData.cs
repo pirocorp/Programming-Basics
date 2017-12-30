b79561f9d53bbd1f484188231f56f88ef1c80c8e
pirocorp <pirocorp@abv.bg>
2017-12-30 15:54:40 +0100

    Employee Data Write a program to read data about an employee and print it on the console with the appropriate formatting.

---
+++ CsharpBasicProblems/EmployeeData/EmployeeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeData
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var employeeID = int.Parse(Console.ReadLine());
+            decimal montlySalary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeID:D8}");
+            Console.WriteLine($"Salary: {montlySalary:F2}");
+        }
+    }
+}
