bb7c3ad39b382ce0fbf55df7a187632bd0facdf0
pirocorp <pirocorp@abv.bg>
2017-12-30 14:51:07 +0100

    AnimalType Complex conditions problem programming basics exercise

---
+++ ComplexConditions/AnimalType/AnimalType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnimalType
+{
+    class AnimalType
+    {
+        static void Main(string[] args)
+        {
+            var animal = Console.ReadLine();
+            switch (animal)
+            {
+                case "dog": Console.WriteLine("mammal"); ; break;
+                case "crocodile":
+                case "tortoise":
+                case "snake": Console.WriteLine("reptile"); break;
+                default: Console.WriteLine("unknow"); break;
+            }
+        }
+    }
+}
