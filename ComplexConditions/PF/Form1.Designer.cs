902070bd6c16b26bfdd4fc199830a479e642064d
pirocorp <pirocorp@abv.bg>
2017-12-30 14:54:05 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/PF/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace PF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelLocation = new System.Windows.Forms.Label();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.labelPoint = new System.Windows.Forms.Label();
+            this.numericUpDownY2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownX2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownY1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownX1 = new System.Windows.Forms.NumericUpDown();
+            this.labelY2 = new System.Windows.Forms.Label();
+            this.labelX2 = new System.Windows.Forms.Label();
+            this.labelY1 = new System.Windows.Forms.Label();
+            this.labelX1 = new System.Windows.Forms.Label();
+            this.labelRectangle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(-73, -42);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(627, 322);
+            this.pictureBox.TabIndex = 33;
+            this.pictureBox.TabStop = false;
+            // 
+            // labelLocation
+            // 
+            this.labelLocation.BackColor = System.Drawing.Color.PaleGreen;
+            this.labelLocation.Location = new System.Drawing.Point(-188, 249);
+            this.labelLocation.Name = "labelLocation";
+            this.labelLocation.Size = new System.Drawing.Size(45, 11);
+            this.labelLocation.TabIndex = 32;
+            this.labelLocation.Text = "Result";
+            this.labelLocation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(-188, 210);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(45, 11);
+            this.buttonDraw.TabIndex = 31;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.DecimalPlaces = 2;
+            this.numericUpDownY.Location = new System.Drawing.Point(-140, 167);
+            this.numericUpDownY.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownY.TabIndex = 30;
+            this.numericUpDownY.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(-171, 169);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(24, 13);
+            this.labelY.TabIndex = 29;
+            this.labelY.Text = "y = ";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(-171, 137);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(24, 13);
+            this.labelX.TabIndex = 28;
+            this.labelX.Text = "x = ";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.DecimalPlaces = 2;
+            this.numericUpDownX.Location = new System.Drawing.Point(-140, 135);
+            this.numericUpDownX.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownX.TabIndex = 27;
+            this.numericUpDownX.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // labelPoint
+            // 
+            this.labelPoint.AutoSize = true;
+            this.labelPoint.Location = new System.Drawing.Point(-188, 112);
+            this.labelPoint.Name = "labelPoint";
+            this.labelPoint.Size = new System.Drawing.Size(34, 13);
+            this.labelPoint.TabIndex = 26;
+            this.labelPoint.Text = "Point:";
+            // 
+            // numericUpDownY2
+            // 
+            this.numericUpDownY2.DecimalPlaces = 2;
+            this.numericUpDownY2.Location = new System.Drawing.Point(-140, 83);
+            this.numericUpDownY2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownY2.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownY2.Name = "numericUpDownY2";
+            this.numericUpDownY2.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownY2.TabIndex = 25;
+            this.numericUpDownY2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownX2
+            // 
+            this.numericUpDownX2.DecimalPlaces = 2;
+            this.numericUpDownX2.Location = new System.Drawing.Point(-140, 51);
+            this.numericUpDownX2.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownX2.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownX2.Name = "numericUpDownX2";
+            this.numericUpDownX2.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownX2.TabIndex = 24;
+            this.numericUpDownX2.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownY1
+            // 
+            this.numericUpDownY1.DecimalPlaces = 2;
+            this.numericUpDownY1.Location = new System.Drawing.Point(-140, 19);
+            this.numericUpDownY1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownY1.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownY1.Name = "numericUpDownY1";
+            this.numericUpDownY1.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownY1.TabIndex = 23;
+            this.numericUpDownY1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            -2147483648});
+            // 
+            // numericUpDownX1
+            // 
+            this.numericUpDownX1.DecimalPlaces = 2;
+            this.numericUpDownX1.Location = new System.Drawing.Point(-140, -13);
+            this.numericUpDownX1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownX1.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownX1.Name = "numericUpDownX1";
+            this.numericUpDownX1.Size = new System.Drawing.Size(10, 20);
+            this.numericUpDownX1.TabIndex = 22;
+            this.numericUpDownX1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // labelY2
+            // 
+            this.labelY2.AutoSize = true;
+            this.labelY2.Location = new System.Drawing.Point(-185, 85);
+            this.labelY2.Name = "labelY2";
+            this.labelY2.Size = new System.Drawing.Size(30, 13);
+            this.labelY2.TabIndex = 21;
+            this.labelY2.Text = "y2 = ";
+            // 
+            // labelX2
+            // 
+            this.labelX2.AutoSize = true;
+            this.labelX2.Location = new System.Drawing.Point(-184, 54);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(30, 13);
+            this.labelX2.TabIndex = 20;
+            this.labelX2.Text = "x2 = ";
+            // 
+            // labelY1
+            // 
+            this.labelY1.AutoSize = true;
+            this.labelY1.Location = new System.Drawing.Point(-184, 21);
+            this.labelY1.Name = "labelY1";
+            this.labelY1.Size = new System.Drawing.Size(30, 13);
+            this.labelY1.TabIndex = 19;
+            this.labelY1.Text = "y1 = ";
+            // 
+            // labelX1
+            // 
+            this.labelX1.AutoSize = true;
+            this.labelX1.Location = new System.Drawing.Point(-184, -13);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(27, 13);
+            this.labelX1.TabIndex = 18;
+            this.labelX1.Text = "x1 =";
+            // 
+            // labelRectangle
+            // 
+            this.labelRectangle.AutoSize = true;
+            this.labelRectangle.Location = new System.Drawing.Point(-188, -45);
+            this.labelRectangle.Name = "labelRectangle";
+            this.labelRectangle.Size = new System.Drawing.Size(59, 13);
+            this.labelRectangle.TabIndex = 17;
+            this.labelRectangle.Text = "Rectangle:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 261);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.labelLocation);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.labelPoint);
+            this.Controls.Add(this.numericUpDownY2);
+            this.Controls.Add(this.numericUpDownX2);
+            this.Controls.Add(this.numericUpDownY1);
+            this.Controls.Add(this.numericUpDownX1);
+            this.Controls.Add(this.labelY2);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelY1);
+            this.Controls.Add(this.labelX1);
+            this.Controls.Add(this.labelRectangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelLocation;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.Label labelPoint;
+        private System.Windows.Forms.NumericUpDown numericUpDownY2;
+        private System.Windows.Forms.NumericUpDown numericUpDownX2;
+        private System.Windows.Forms.NumericUpDown numericUpDownY1;
+        private System.Windows.Forms.NumericUpDown numericUpDownX1;
+        private System.Windows.Forms.Label labelY2;
+        private System.Windows.Forms.Label labelX2;
+        private System.Windows.Forms.Label labelY1;
+        private System.Windows.Forms.Label labelX1;
+        private System.Windows.Forms.Label labelRectangle;
+    }
+}
+
