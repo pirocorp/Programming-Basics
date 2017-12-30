15d2ead98225b3090b5ba09e24f79ef09a1fdcdd
pirocorp <pirocorp@abv.bg>
2017-12-30 18:04:30 +0100

    Prints the time after 30 minutes.

---
+++ CsharpBasicProblems/Back30Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 30;
+            
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+
+            if (hours >= 24)
+            {
+                hours = hours - 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
