3bbd1b506b455b2ee6d747468080d8160eb0df28
pirocorp <pirocorp@abv.bg>
2017-12-30 14:52:35 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/Cinema/Cinema.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Cinema
+    {
+        static void Main(string[] args)
+        {
+            var Type = Console.ReadLine().ToLower();
+            var r = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var seats = r * c;
+            var price = -1.00;
+
+            if (Type == "remiere") price = 12.00;
+            else if (Type == "normal") price = 7.5;
+            else if (Type == "discount") price = 5.0;
+
+            if (price >0)
+            {
+                Console.WriteLine("{0:f2} leva", seats * price);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
