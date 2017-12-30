abc6c0cdde9cf7e302b084b392a4a0f615c5c957
pirocorp <pirocorp@abv.bg>
2017-12-30 14:54:45 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/PointFigureForm/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PointFigureForm
+{
+    partial class FormPointAndRectangle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPointInFigure = new System.Windows.Forms.Label();
+            this.labelH = new System.Windows.Forms.Label();
+            this.numericUpDownH = new System.Windows.Forms.NumericUpDown();
+            this.labelPoint = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.labelY = new System.Windows.Forms.Label();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPointInFigure
+            // 
+            this.labelPointInFigure.AutoSize = true;
+            this.labelPointInFigure.Location = new System.Drawing.Point(13, 13);
+            this.labelPointInFigure.Name = "labelPointInFigure";
+            this.labelPointInFigure.Size = new System.Drawing.Size(116, 20);
+            this.labelPointInFigure.TabIndex = 0;
+            this.labelPointInFigure.Text = "Point In Figure:";
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Location = new System.Drawing.Point(14, 38);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(35, 20);
+            this.labelH.TabIndex = 1;
+            this.labelH.Text = "h = ";
+            // 
+            // numericUpDownH
+            // 
+            this.numericUpDownH.Location = new System.Drawing.Point(55, 36);
+            this.numericUpDownH.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownH.Name = "numericUpDownH";
+            this.numericUpDownH.Size = new System.Drawing.Size(70, 26);
+            this.numericUpDownH.TabIndex = 2;
+            this.numericUpDownH.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDownH.ValueChanged += new System.EventHandler(this.numericUpDownH_ValueChanged);
+            // 
+            // labelPoint
+            // 
+            this.labelPoint.AutoSize = true;
+            this.labelPoint.Location = new System.Drawing.Point(17, 83);
+            this.labelPoint.Name = "labelPoint";
+            this.labelPoint.Size = new System.Drawing.Size(49, 20);
+            this.labelPoint.TabIndex = 3;
+            this.labelPoint.Text = "Point:";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(17, 114);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(33, 20);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "x = ";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.InterceptArrowKeys = false;
+            this.numericUpDownX.Location = new System.Drawing.Point(55, 113);
+            this.numericUpDownX.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(70, 26);
+            this.numericUpDownX.TabIndex = 5;
+            this.numericUpDownX.ValueChanged += new System.EventHandler(this.numericUpDownX_ValueChanged);
+            this.numericUpDownX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDownX_KeyDown);
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(18, 152);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(33, 20);
+            this.labelY.TabIndex = 6;
+            this.labelY.Text = "y = ";
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.InterceptArrowKeys = false;
+            this.numericUpDownY.Location = new System.Drawing.Point(55, 151);
+            this.numericUpDownY.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(70, 26);
+            this.numericUpDownY.TabIndex = 7;
+            this.numericUpDownY.ValueChanged += new System.EventHandler(this.numericUpDownY_ValueChanged);
+            this.numericUpDownY.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDownY_KeyDown);
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(15, 196);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(109, 33);
+            this.buttonDraw.TabIndex = 8;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.PaleGreen;
+            this.labelResult.Location = new System.Drawing.Point(15, 242);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(109, 31);
+            this.labelResult.TabIndex = 9;
+            this.labelResult.Text = "Result";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(135, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(537, 347);
+            this.pictureBox.TabIndex = 10;
+            this.pictureBox.TabStop = false;
+            // 
+            // FormPointAndRectangle
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(684, 371);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.labelPoint);
+            this.Controls.Add(this.numericUpDownH);
+            this.Controls.Add(this.labelH);
+            this.Controls.Add(this.labelPointInFigure);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(500, 410);
+            this.Name = "FormPointAndRectangle";
+            this.Text = "Point in the Figure";
+            this.Load += new System.EventHandler(this.FormPointAndRectangle_Load);
+            this.Resize += new System.EventHandler(this.FormPointAndRectangle_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPointInFigure;
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.NumericUpDown numericUpDownH;
+        private System.Windows.Forms.Label labelPoint;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ ComplexConditions/PointFigureForm/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PointFigureForm
+{
+    public partial class FormPointAndRectangle : Form
+    {
+             
+        public FormPointAndRectangle()
+        {
+        InitializeComponent();
+        }
+
+        private void FormPointAndRectangle_Load(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownH_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownX_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownY_ValueChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            int h = (int)this.numericUpDownH.Value;
+            int x = (int)this.numericUpDownX.Value;
+            int y = (int)this.numericUpDownY.Value;
+
+            int z = DisplayPointLocation(h, x, y);
+
+            // Calculate the scale factor (ratio) for the diagram holding the
+            // rectangle and point in order to fit them well in the picture box
+
+            //double ratio;
+            //int offset = 0;
+            //ratio = pictureBox.Width / 4 * h;
+            //ratio = Math.Floor(ratio);
+
+            //int r = 1; //(int)ratio;
+
+            int x1 = 0;
+            int y1 = 0;
+            int w1 = 3 * h *10;
+            int h1 = h * 10;
+            var rect = new Rectangle(x1, y1, w1, h1);
+            int x2 = h * 10;
+            int y2 = 4 * h * 10;
+            int w2 = h * 10;
+            int h2 = 3* h * 10;
+            var rect2 = new Rectangle(x2, x2, w2, h2);
+
+            // Calculate the scalled point coordinates
+            var pointX = x;
+            var pointY = y;
+            var pointRect = new Rectangle(pointX * 10 - 1, pointY * 10 - 1, 2, 2);
+
+            // Draw the rectangle and point
+            if(pictureBox.Width > 0 && pictureBox.Height > 0)
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (var g = Graphics.FromImage(pictureBox.Image))
+            {
+                // Draw diagram background (white area)
+                g.Clear(Color.White);
+
+                // Draw the rectangle (scalled to the picture box size)
+                var pen = new Pen(Color.Blue, 3);
+                if (z == 4 || z == 3) { pen = new Pen(Color.Orange, 3);}
+                SolidBrush Brush = new SolidBrush(Color.PaleGreen);
+                if (z == 1) g.FillRectangle(Brush, rect);
+                g.DrawRectangle(pen, rect);
+
+                pen = new Pen(Color.Red, 3);
+                if (z == 4 || z == 3) { pen = new Pen(Color.Orange, 3);}
+                if (z == 2) g.FillRectangle(Brush, rect2);
+                g.DrawRectangle(pen, rect2);
+
+                // Draw the point (scalled to the picture box size)
+                pen = new Pen(Color.DarkBlue, 5);
+                g.DrawEllipse(pen, pointRect);
+            }
+
+
+
+        }
+
+        private decimal Min(decimal val1, decimal val2, decimal val3)
+        {
+            return Math.Min(val1, Math.Min(val2, val3));
+        }
+
+        private decimal Max(decimal val1, decimal val2, decimal val3)
+        {
+            return Math.Max(val1, Math.Max(val2, val3));
+        }
+
+        private int DisplayPointLocation(int h, int x, int y)
+        {
+            bool inRectangle1 = ((x > 0 && x < 3 * h) && (y > 0 && y < h)); // <=> ((x > 0 && y > 0) && (x < 3h && y < h))
+            bool inRectangle2 = ((x > h && x < 2 * h) && (y > h && y < 4 * h)); // <=> ((x > h && y > h) && (x < 2h && y < 4h))
+            bool outRectangle1 = ((x < 0 || x > 3 * h) || (y < 0 || y > h));
+            bool outRectangle2 = ((x < h || x > 2 * h) || (y < h || y > 4 * h));
+
+            bool commonBorder = ((x >= h && x <= 2 * h) && y == h);
+
+            if (outRectangle1 && outRectangle2)
+            {
+                this.labelResult.Text = "Outside";
+                this.labelResult.BackColor = Color.LightSalmon;
+                return 0;
+            }
+            else if (inRectangle1 || inRectangle2)
+            {
+                this.labelResult.Text = "Inside";
+                this.labelResult.BackColor = Color.LightGreen;
+                if(inRectangle1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else if (commonBorder)
+            {
+                this.labelResult.Text = "Common";
+                this.labelResult.BackColor = Color.LightPink;
+                return 3;
+            }
+            else
+            {
+                this.labelResult.Text = "Border";
+                this.labelResult.BackColor = Color.Gold;
+                return 4;
+            }
+        }
+
+        private void FormPointAndRectangle_Resize(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void numericUpDownY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void numericUpDownX_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (this.numericUpDownX.Value < this.pictureBox.Width / 10) this.numericUpDownX.Value++;
+                else this.numericUpDownX.Value = 0;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (this.numericUpDownX.Value > 0) this.numericUpDownX.Value--;
+                else
+                {
+                    this.numericUpDownX.Value = this.pictureBox.Width / 10;
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (this.numericUpDownY.Value > 0) this.numericUpDownY.Value--;
+                else
+                {
+                    this.numericUpDownY.Value = this.pictureBox.Height / 10;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (this.numericUpDownY.Value < this.pictureBox.Height / 10) this.numericUpDownY.Value++;
+                else this.numericUpDownY.Value = 0;
+            }
+
+            Draw();
+        }
+
+        private void numericUpDownY_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                if (this.numericUpDownX.Value < this.pictureBox.Width / 10) this.numericUpDownX.Value++;
+                else this.numericUpDownX.Value = 0;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (this.numericUpDownX.Value > 0) this.numericUpDownX.Value--;
+                else
+                {
+                    this.numericUpDownX.Value = this.pictureBox.Width / 10;
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (this.numericUpDownY.Value > 0) this.numericUpDownY.Value--;
+                else
+                {
+                    this.numericUpDownY.Value = this.pictureBox.Height / 10;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (this.numericUpDownY.Value < this.pictureBox.Height / 10) this.numericUpDownY.Value++;
+                else this.numericUpDownY.Value = 0;
+            }
+
+            Draw();
+        }
+
+    }
+}
