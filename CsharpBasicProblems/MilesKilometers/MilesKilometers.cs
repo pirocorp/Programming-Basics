d54068f1dae26dfcc049854d37a15cb05a114065
pirocorp <pirocorp@abv.bg>
2017-12-30 16:48:22 +0100

    Write a program, which converts miles to kilometers. Format the output to the 2nd decimal place.

---
+++ CsharpBasicProblems/MilesKilometers/MilesKilometers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MilesKilometers
+{
+    class MilesKilometers
+    {
+        static void Main(string[] args)
+        {
+            double mile = 1.60934;
+            double miles = double.Parse(Console.ReadLine());
+            double result = mile * miles;
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
