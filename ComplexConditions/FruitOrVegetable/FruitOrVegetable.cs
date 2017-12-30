5adc6608111ecf2718630db592017ff0975acfc6
pirocorp <pirocorp@abv.bg>
2017-12-30 14:53:13 +0100

    Complex conditions problem programming basics exercise

---
+++ ComplexConditions/FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FruitOrVegetable
+{
+    class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().ToLower();
+            if (s == "banana" || s == "apple" || s == "kiwi" ||
+                s == "cherry" || s == "lemon" || s == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (s == "tomato" || s == "cucumber" || s == "pepper" || s == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
