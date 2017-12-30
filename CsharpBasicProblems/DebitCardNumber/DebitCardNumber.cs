5c9051ef54ed9f91aee54a3ea31cc92c657aab03
pirocorp <pirocorp@abv.bg>
2017-12-30 16:37:02 +0100

    Write a program, which receives 4 integers on the console and prints them in 4-digit debit card format.

---
+++ CsharpBasicProblems/DebitCardNumber/DebitCardNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DebitCardNumber
+{
+    class DebitCardNumber
+    {
+        static void Main()
+        {
+            var inputString = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                inputString += $"{number:d4}";
+                if (i != 3) inputString += " ";
+            }
+            Console.WriteLine(inputString);
+
+        }
+    }
+}
