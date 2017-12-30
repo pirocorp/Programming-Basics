392d48fe1a33db6304606e3517fe9ecf902e69f3
pirocorp <pirocorp@abv.bg>
2017-12-30 19:05:02 +0100

    Write a program that prints the next n odd numbers (starting from 1) and on the last row prints the sum of them.

---
+++ CsharpBasicProblems/SumOddNumbers/SumOddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumOddNumbers
+{
+    class SumOddNumbers
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var x = int.Parse(Console.ReadLine());
+
+            if (x <= 10)
+            {
+                for (int i = x; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{n} X {x} = {n * x}");
+            }
+        }
+    }
+}
