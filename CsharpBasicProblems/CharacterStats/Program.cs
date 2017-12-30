86bf3a3efafabd157657ae9d8f82df452bdd9344
pirocorp <pirocorp@abv.bg>
2017-12-30 17:09:47 +0100

    Write a program, which displays information about a video game character. You will receive their name, current health, maximum health, current energy and maximum energy on separate lines. The current values will always be valid (equal or lower than their respective max values).

---
+++ CsharpBasicProblems/CharacterStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var helth = int.Parse(Console.ReadLine());
+            var maxHelth = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var maxEnergy = int.Parse(Console.ReadLine());
+
+            var leftHelth = maxHelth - helth;
+            var leftEnergy = maxEnergy - energy;
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("Health: |{0}{1}|", new string('|', helth), new string('.', leftHelth));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', energy), new string('.', leftEnergy));
+        }
+    }
+}
