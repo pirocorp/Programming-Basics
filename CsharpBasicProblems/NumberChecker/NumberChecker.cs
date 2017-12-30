ca6577329e65313dde6a48f69099017a9118296d
pirocorp <pirocorp@abv.bg>
2017-12-30 19:17:15 +0100

    Write a program, which reads an input from the console and prints "It is a number." if it’s a number. If it is not write "Invalid input!"

---
+++ CsharpBasicProblems/NumberChecker/NumberChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberChecker
+{
+    class NumberChecker
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double n = int.Parse(Console.ReadLine());
+                Console.WriteLine("It is a number.");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
