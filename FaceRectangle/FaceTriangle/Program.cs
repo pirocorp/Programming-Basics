ec1e82ea58af5709d6158dedaf1088b475641a94
pirocorp <pirocorp@abv.bg>
2017-12-30 14:45:15 +0100

    Face Rectangle Programming basics exercise

---
+++ FaceRectangle/FaceTriangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FaceRectangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
