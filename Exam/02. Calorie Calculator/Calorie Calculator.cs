2305bf40eff30d7ef6a511d6406f36f9f6790389
pirocorp <pirocorp@abv.bg>
2018-05-02 12:34:16 +0200

    "28.04.2018 EXAM"

---
+++ Exam/02. Calorie Calculator/Calorie Calculator.cs
@@ -0,0 +1,49 @@
+﻿namespace _02.Calorie_Calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class CalorieCalculator
+    {
+        public static void Main()
+        {
+            var gender = Console.ReadLine();
+            var kg = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine()); //In meters
+            var age = int.Parse(Console.ReadLine());
+            var inputString = Console.ReadLine();
+
+            var bhm = 0.0;
+
+            if (gender == "m")
+            {
+                bhm = 66 + (13.7 * kg) + (5 * height * 100) - (6.8 * age);
+            }
+            else
+            {
+                bhm = 655 + (9.6 * kg) + (1.8 * height * 100) - (4.7 * age);
+            }
+
+            switch (inputString)
+            {
+                case "sedentary":
+                    bhm *= 1.2;
+                    break;
+                case "lightly active":
+                    bhm *= 1.375;
+                    break;
+                case "moderately active":
+                    bhm *= 1.55;
+                    break;
+                case "very active":
+                    bhm *= 1.725;
+                    break;
+            }
+
+            Console.WriteLine($"To maintain your current weight you will need {Math.Ceiling(bhm)} calories per day.");
+        }
+    }
+}
---
+++ Exam/06. Sum And Product/Sum And Product.cs
@@ -0,0 +1,45 @@
+﻿namespace _06.Sum_And_Product
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class SumAndProduct
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (var a = 1; a <= 9; a++)
+            {
+                for (var b = 9; b >= a; b--)
+                {
+                    for (var c = 0; c <= 9; c++)
+                    {
+                        for (var d = 9; d >= c; d--)
+                        {
+                            var sum = a + b + c + d;
+                            var product = a * b * c * d;
+
+                            if (sum == product && n % 10 == 5)
+                            {
+                                Console.WriteLine($"{a}{b}{c}{d}");
+                                return;
+                            }
+
+                            if (product / sum == 3 && n % 3 == 0)
+                            {
+                                Console.WriteLine($"{d}{c}{b}{a}");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Nothing found");
+        }
+    }
+}
